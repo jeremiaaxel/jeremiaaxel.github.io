4127103a35fdef67908a6e5e96ee0b57166d9102
Jeremia Axel <jeremiaaxelb@gmail.com>
2023-06-26 18:31:11 +0700

    Add notification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,31 @@
 	import ProjectsComponent from '../components/projects.svelte';
 	import ProfileComponent from '../components/profile.svelte';
 	import SkillsComponent from '../components/skills.svelte';
-	import SkillsDescription from '../components/skills-description.svelte';
+	import SkillsDescriptionComponent from '../components/skills-description.svelte';
+	import NotificationComponent from '../components/notification.svelte';
 
 	let profile: Profile = dataJson.profile;
+
+	const priorities = ['internship', 'school', 'personal'];
 	let projects: Project[] = dataJson.projects;
+
 	let dynamicSkills: string[] = extractTechStacks(projects);
 	let skillsDescription = dataJson.skillsDescription;
-	const priorities = ['internship', 'school', 'personal'];
+
+	const notification = {
+		title: 'Work In Progress',
+		detail: 'This website is still under development. But the content is ready.',
+	};
 </script>
 
 <section
 	class="flex flex-col my-20 space-y-5
-		   md:container xl:mx-80 lg:mx-40 sm:mx-20 md:px-5"
+		   md:container mx-auto
+		   md:w-3/5 w-full"
 >
+	<NotificationComponent data={notification} />
 	<ProfileComponent data={profile} />
 	<ProjectsComponent data={projects} priorities={priorities} />
 	<SkillsComponent data={dynamicSkills} />
-	<SkillsDescription data={skillsDescription} />
+	<SkillsDescriptionComponent data={skillsDescription} />
 </section>
