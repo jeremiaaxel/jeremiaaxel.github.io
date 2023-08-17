79c07d40093332226717da6097ad3a60a78b8d7a
Jeremia Axel <jeremiaaxelb@gmail.com>
2023-08-17 14:33:24 +0700

    add label to buttons

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -2,7 +2,7 @@
 	import type { Profile } from '$lib/types/profile';
 
 	export let data: Profile;
-	data.links.forEach(link => {
+	data.links.forEach((link) => {
 		if (link.icon) {
 			let lastSlash = link.icon.lastIndexOf('/');
 			link.iconAlt = link.icon.substring(lastSlash + 1);
@@ -15,9 +15,14 @@
 	<p>{data.description}</p>
 	<section class="links flex flex-row">
 		{#each data.links as link}
-			<a class="p-2 mx-1 link rounded-md bg-neutral-100 hover:bg-neutral-50" href={link.url} target="_blank">
+			<a
+				class="p-2 mx-1 link rounded-md bg-neutral-100 hover:bg-neutral-50"
+				href={link.url}
+				target="_blank"
+				title={link.name}
+			>
 				{#if link.icon}
-					<img width="25" height="25" src={link.icon} alt="{link.iconAlt}"/>
+					<img width="25" height="25" src={link.icon} alt={link.iconAlt} />
 				{:else}
 					{link.name}
 				{/if}
