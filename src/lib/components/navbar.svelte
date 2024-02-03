ad75627c278196cb94d49191310eb5668f3ab374
Jeremia Axel <jeremiaaxelb@gmail.com>
2024-02-04 01:28:46 +0700

    feat: navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import * as Menubar from '$lib/components/ui/menubar';
+	import Profile from '$lib/data/profile';
+	import { Badge } from '$lib/components/ui/badge';
+</script>
+
+<section>
+	<Menubar.Root>
+		<Menubar.Menu>
+			<Menubar.Trigger>
+				<a href="/">Home</a>
+			</Menubar.Trigger>
+		</Menubar.Menu>
+		<Menubar.Menu>
+			<Menubar.Trigger class="cursor-pointer">Contact</Menubar.Trigger>
+			<Menubar.Content>
+				{#each Profile.links as link}
+					<Menubar.Item class="cursor-pointer">
+						<a href={link.url} class="capitalize flex gap-2" target="_blank">
+							<svelte:component this={link.icon} class="size-4" />
+							{link.name}
+						</a>
+					</Menubar.Item>
+				{/each}
+			</Menubar.Content>
+		</Menubar.Menu>
+		<Menubar.Menu>
+			<Menubar.Trigger class="bg-slate-50 whitespace-nowrap">
+				<a href="https://moneybackward.jeremiaaxel.my.id" target="_blank">
+					Money Backward
+					<Badge class="ml-1 bg-gray-400 text-white outline-none">New</Badge>
+				</a>
+      </Menubar.Trigger>
+		</Menubar.Menu>
+	</Menubar.Root>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
 	import dataJson from '$lib/data/data.json';
 	import profileJson from '$lib/data/profile';
-	import sidebarJson from '$lib/data/sidebar.json';
 
 	import { containsSkills, extractTechStacks } from '$lib/utils/projects';
 	import { extractTechnologies } from '$lib/utils/technologies';
@@ -12,27 +11,26 @@
 	import ProjectsComponent from '$lib/components/projects.svelte';
 	import ProfileComponent from '$lib/components/profile.svelte';
 	import SkillsComponent from '$lib/components/skills.svelte';
-	import TechnologiesComponent from '$lib/components/technologies.svelte';
-	// import NotificationComponent from '../components/notification.svelte';
-	// import Sidebar from '../components/sidebar.svelte';
+	import NavbarComponent from '$lib/components/navbar.svelte';
 	import { onMount } from 'svelte';
 	import { fade, fly } from 'svelte/transition';
 	import { linear } from 'svelte/easing';
 
-	const projectPriorities = ['fulltime', 'freelance', 'personal', 'internship', 'volunteering', 'school'];
+	const projectPriorities = [
+		'fulltime',
+		'freelance',
+		'personal',
+		'internship',
+		'volunteering',
+		'school'
+	];
 
 	const profile: Profile = profileJson;
 	const { projects, technologies } = dataJson;
-	const { items: sidebarItems, brand } = sidebarJson;
 
 	const skillsFromProjects: string[] = extractTechStacks(projects as Project[]);
 	const skillsFromTechnologies: string[] = extractTechnologies(technologies);
 	const skills = [...new Set(skillsFromProjects.concat(skillsFromTechnologies))].sort();
-
-	const notification = {
-		title: 'Work In Progress',
-		detail: 'This website is still under development. But the contents are ready.'
-	};
 
 	let skillsClicked = {
 		latestClick: 0,
@@ -57,52 +55,43 @@
 			? projects
 			: projects.filter((project) => containsSkills(project, [...skillsFilter]));
 
-	function scrollHandler(event: Event) {
-		const target = event.target as HTMLElement;
-		const scrollHeight = target.scrollHeight;
-		const scrollTop = target.scrollTop;
-		const clientHeight = target.clientHeight;
-		const scrollBottom = scrollHeight - scrollTop - clientHeight;
-
-		if (scrollBottom < 1) {
-			console.log('bottom');
-		}
-	}
-
 	let ready = false;
 	onMount(() => (ready = true));
 </script>
 
 <svelte:head>
 	<title>Jeremia Axel's Portofolio Web</title>
-	<meta content="Jeremia Axel's Portofolio Web to show projects and/or experiences." name="description" />
+	<meta
+		content="Jeremia Axel's Portofolio Web to show projects and/or experiences."
+		name="description"
+	/>
 	<link href="" rel="icon" />
 </svelte:head>
 
 <section
-	class="flex flex-auto flex-col my-5 space-y-4
+>
+	<NavbarComponent />
+	<section
+		class="flex flex-auto flex-col my-5 space-y-4
 		   md:container mx-auto
 		   px-10 py-5 rounded-lg"
-	on:scroll={scrollHandler}
->
-	<!-- <NotificationComponent data={notification} /> -->
-	<!-- <Sidebar brand={brand} data={sidebarItems} /> -->
-	{#if ready}
-		<div transition:fade={{ duration: 500, easing: linear }}>
-			<ProfileComponent data={profile} />
-		</div>
+	>
+		{#if ready}
+			<div transition:fade={{ duration: 500, easing: linear }}>
+				<ProfileComponent data={profile} />
+			</div>
 
-		<div
-			transition:fly={{ delay: 400, duration: 700, easing: linear, y: 100, opacity: 0 }}
-			class="flex flex-col md:flex-row-reverse gap-5"
-		>
-			<section id="sidebar" class="md:w-3/12 md:sticky md:top-6 h-fit">
-				<SkillsComponent data={skills} bind:skillsClicked />
-			</section>
-			<section id="main-content" class="md:w-9/12">
-				<ProjectsComponent bind:data={filteredProjects} priorities={projectPriorities} />
-				<!-- <TechnologiesComponent data={technologies} /> -->
-			</section>
-		</div>
-	{/if}
+			<div
+				transition:fly={{ delay: 400, duration: 700, easing: linear, y: 100, opacity: 0 }}
+				class="flex flex-col md:flex-row-reverse gap-5"
+			>
+				<section id="sidebar" class="md:w-3/12 md:sticky md:top-6 h-fit">
+					<SkillsComponent data={skills} bind:skillsClicked />
+				</section>
+				<section id="main-content" class="md:w-9/12">
+					<ProjectsComponent bind:data={filteredProjects} priorities={projectPriorities} />
+				</section>
+			</div>
+		{/if}
+	</section>
 </section>
