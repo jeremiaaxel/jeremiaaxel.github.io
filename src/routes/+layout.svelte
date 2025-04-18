eff625e8b821ddc8f52e1404b73138aed750dd1c
Jeremia Axel <jeremiaaxelb@gmail.com>
2025-04-18 23:36:16 +0700

    feat: integrate posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
   import '../app.css';
   import { ModeWatcher } from 'mode-watcher';
   import { Toaster } from '$lib/components/ui/sonner';
+  import posthog from 'posthog-js';
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
+  import { PUBLIC_POSTHOG_API_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
+
+  onMount(() => {
+    if (browser) {
+      posthog.init(PUBLIC_POSTHOG_API_KEY, {
+        api_host: PUBLIC_POSTHOG_HOST,
+        person_profiles: 'always',
+      });
+    }
+  });
 </script>
 
 <ModeWatcher />
